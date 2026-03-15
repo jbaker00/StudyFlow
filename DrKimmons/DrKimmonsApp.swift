878144125a7805d0eae150d3b911553caf67232c
James Baker <jbaker00@users.noreply.github.com>
2026-03-15 09:07:52 -0700

    Initial Commit

---
+++ DrKimmons/DrKimmonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrKimmonsApp.swift
+//  DrKimmons
+//
+//  Created by James Baker on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct DrKimmonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
